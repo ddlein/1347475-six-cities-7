860537614f57dac7586298adbaad050025c2717c
Дима <70834132+ddlein@users.noreply.github.com>
2021-06-02 22:20:44 +0300

    Задание 1.3

--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Settings = {
+  PROMO_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      promoCount={Settings.PROMO_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
+
+export {Settings};
